feeb22f3640294b864388a7290b1f48004c39a5c
jsbaidwan <cgc.jsbaidwan@gmail.com>
2018-02-08 14:08:57 +0530

    checkout commit

--- mywebapp/Startup.cs
+++ mywebapp/Startup.cs
@@ -15,7 +15,6 @@ namespace mywebapp
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,11 +25,9 @@ namespace mywebapp
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseMvc();
-
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hey!");
+                await context.Response.WriteAsync("Hello World!");
             });
         }
     }
